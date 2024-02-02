3ac19de625fb88216b7ea196312d38e7218e38c5
Lojhar <hajaraitcharmou598@gmail.com>
2024-02-01 18:18:48 -0800

    adding

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -8,8 +8,6 @@
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node == NULL || node->parent != NULL)
-		return (0);
 
-	return (1);
+	return (node == NULL && node->parent != NULL);
 }
